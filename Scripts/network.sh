7a627f062f62d27ff059cfea9ef2dbe34b444153
LanderU <lander.usategui@gmail.com>
2015-06-28 11:41:05 +0200

    More changes

--- Scripts/network.sh
+++
@@ -1,7 +0,0 @@
-#lander@erlerobot.com
-echo "Connect the ethernet wire into the ERLE-BRAIN, at port (if it is not already)"
-sleep 15
-sudo ifconfig eth0 up
-echo "Setting your network, please wait..."
-sudo dhclient eth0
-echo "Network address: `ifconfig eth0 2>/dev/null`"
